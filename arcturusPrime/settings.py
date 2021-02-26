fa40292cb5d12d8d45389cc5723d43945adf0350
adammdekoning <adam.m.dekoning@gmail.com>
2021-02-26 08:13:18 +0000

    Preparing to set up API backend

--- arcturusPrime/settings.py
+++ arcturusPrime/settings.py
@@ -39,8 +39,17 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.humanize',
     'frontEnd',
+    'rest_framework',
 
 ]
+
+REST_FRAMEWORK = {
+    # Use Django's standard `django.contrib.auth` permissions,
+    # or allow read-only access for unauthenticated users.
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
